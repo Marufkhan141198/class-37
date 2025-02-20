1a496dcbe7a37f07d121c2e7d5b394c6a090e2dc
Marufkhan141198 <khannmaruf141198@gmail.com>
2025-02-20 19:18:18 +0600

    js

---
+++ script.js
@@ -0,0 +1,11 @@
+let twoDimensionalArray = [
+    [4, 31, 40, 23],
+    [10, 20, 56, 54],
+    [200, 300, 78, 90]
+];
+
+console.log(twoDimensionalArray[2][3],[2][1]);
+console.log(twoDimensionalArray[2][1]);
+
+
+
